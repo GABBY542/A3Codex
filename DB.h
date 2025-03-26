1767c6d5775384fc8cb876ede6c402271cd4ed17
Zerus137 <trana47@mymacewan.ca>
2025-03-25 20:29:08 -0600

    Update DB.h

--- DB.h
+++ DB.h
@@ -5,9 +5,9 @@
  * Please do not modify any code in this file!  We will be testing your code
  * under the assumption that the public interface matches this interface.
  *
- * Author: <TODO: Group Member Names>
- * Lab instructor: <TODO: Your lab instructor's name here>
- * Lecture instructor: <TODO: Your lecture instructor's name here>
+ * Author: Gabrielle Phillips, Lara Halawa, Emem Afanide, Andy Tran
+ * Lab instructor: RD Ardy
+ * Lecture instructor: RD Ardy , Dhara Wagh
  */
 
 #ifndef DB_H
@@ -18,12 +18,56 @@
  * WHICH WILL BE FIELDS IN THE STRUCT DataBase BELOW. YOU MAY DECLARE ADDITIONAL
  * STRUCTS AS NEEDED.
  */
-
+# Struct Table, NeighbourhoodTable, AND PicnicTable
 /*
  * The INIT_SIZE is only relevant if you are using arrays for your data structures. 
  * If you are using linked lists you will not need it
  */
-#define INIT_SIZE 5  
+#define INIT_SIZE 5 
+
+typedef struct {
+    unsigned short int tableID;
+    char *TableType;
+}Entries;
+
+typedef struct {
+    unsigned int size;
+    unsigned int capacity;
+    Entries *arr;
+}Table;
+
+typedef struct {
+    unsigned int size;
+    unsigned int capacity;
+    Entries *arr;
+}NeighbourhoodTable;
+
+
+
+typedef struct {
+// holds all the each entry in the .csv file 
+// Maybe change bit size to some (later thing to do?)
+    unsigned short int TableTypeId; 
+    unsigned short int SiteId;
+    unsigned short int TableId;
+    unsigned short int structuralMaterialId;
+    unsigned short int surfaceMaterialId;
+    unsigned short neighbhdId;
+    char *WardId;
+    char *latitude;
+    char *longitude;
+    char *location;
+    char *streetAvenue;
+    char *NeighbourhoodName;
+}ThingsforTable;
+
+typedef struct {
+    unsigned int size;
+    unsigned int capacity;
+    ThingsforTable *TableTable // array of thingsfortable with entry of tabletable
+
+}PicnicTable;
+
 
 
 /*
@@ -43,6 +87,13 @@ typedef struct {
 /* That should be the only global variable declared*/
 /* DB.c should have the definition of this variable*/
 extern DataBase *Db;
+
+// his function takes the name of a .csv file as parameter and creates and populates the Database with the corresponding data set information.
+void importDB(file name);
+
+
+// this function takes the name of a .csv file as parameter and creates a .csv file containing the information of the Database. NOTE: the exported .csv file must be exactly the same as the original .csv file from which the Database was created.
+void exportDB(file name);
 
 
 /*
